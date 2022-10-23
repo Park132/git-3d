2458bfe56c184ee983de69227ff6bad4f167eb56
Park132 <aice7@hanmail.net>
2022-10-23 15:26:32 +0900

    2day -1
    포탑 구현
    발사 구현
    카메라 고정구현
    터레인 조금만 손댐
    --------
    적 인공지능 구현해야함
    데미지 주고받기에 관한 알고리즘 생각해보기

---
+++ Project_Tank/Assets/Script/Bullet_ctrl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_ctrl : MonoBehaviour
+{
+    public GameObject bullet;
+    public float penetrate_possibility = 0.7f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.transform.tag == "Enemy")
+    }
+}
--- Project_Tank/Assets/Script/Player_ctrl.cs
+++ Project_Tank/Assets/Script/Player_ctrl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//ui로 재장전 시간 구현 해야 함
 
 public class Player_ctrl : MonoBehaviour
 {
@@ -8,12 +9,23 @@ public class Player_ctrl : MonoBehaviour
     public GameObject pturret;
     public GameObject pcannon;
     public GameObject bullet_prefab;
+    public GameObject bullet_spawn_point;
+
+    public ParticleSystem bullet_spawn_effect;
+    //연막효과도 넣기
 
     public Camera camera_pos;
 
-    private float movespeed = 15.0f;
-    private float headrotationspeed = 22.0f;
-    private float bodyrotationspeed = 22.0f;
+    public float Health = 100.0f; // 데미지 주는 것과 받는것, 방어력, 도탄 등의 알고리즘 생각해보기 
+    //적의 인공지능도 생각해보기
+
+    [SerializeField]
+    private float world_timer = 0.0f;
+    private float reload_timer = 5.0f;
+    private float firepower = 20000.0f; // 포탄에 가하는 힘
+    private float movespeed = 15.0f; // 탱크 앞 뒤 이동속도
+    private float headrotationspeed = 1.0f; // 포탑 회전속도
+    private float bodyrotationspeed = 22.0f; // 차체 회전속도
     
 
     private bool isMoving = false;
@@ -28,8 +40,12 @@ public class Player_ctrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        world_timer += Time.deltaTime;
+
         BodyMove();
         TurretMove();
+        OnFire();
+        CameraLook();
     }
 
     void BodyMove() // 탱크 이동 V 
@@ -50,7 +66,7 @@ public class Player_ctrl : MonoBehaviour
         this.transform.Rotate(new Vector3(0, y * bodyrotationspeed * Time.deltaTime, 0));
     }
 
-    void TurretMove()// 포탑 구현 쳐다보게하기!!! V
+    void TurretMove()// 포탑 구현 쳐다보게하기!!! delay를 어떻게 줄까 V
     {
         Ray ray = camera_pos.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit_res;
@@ -58,17 +74,30 @@ public class Player_ctrl : MonoBehaviour
         if(Physics.Raycast(ray, out hit_res))
         {
             Vector3 mouseDir = new Vector3(hit_res.point.x, pcannon.transform.position.y, hit_res.point.z) - transform.position;
-            pturret.transform.forward = mouseDir;
+
+            pturret.transform.rotation = Quaternion.LerpUnclamped(pturret.transform.rotation, Quaternion.LookRotation(mouseDir), headrotationspeed * Time.deltaTime);
+
+            //pturret.transform.forward  = mouseDir;
         }
     }
 
-    void OnFire() // 발사 구현하기
+    void OnFire() // 발사 구현하기 V 
     {
+        if (Input.GetMouseButtonDown(0) && world_timer >= reload_timer)
+        {
+            GameObject bullet_prefabs = Instantiate(bullet_prefab, bullet_spawn_point.transform.position, bullet_spawn_point.transform.rotation);
+            ParticleSystem bullet_spawn_effect_prefabs = Instantiate(bullet_spawn_effect, bullet_spawn_point.transform.position, bullet_spawn_point.transform.rotation);
 
+            bullet_prefabs.gameObject.GetComponent<Rigidbody>().AddForce(bullet_spawn_point.transform.forward * firepower);
+            world_timer = 0;
+        }
     }
 
-    void CameraLook()
+    void CameraLook() // 카메라 고정 구현 V
     {
+        Vector3 offset = new Vector3(0, 150, -120);
 
-    } //카메라 아직 구현 못함 
+        camera_pos.transform.position = ptank.transform.position + offset;
+        camera_pos.transform.LookAt(ptank.transform);
+    }
 }
