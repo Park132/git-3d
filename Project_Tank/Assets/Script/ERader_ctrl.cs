c07b8489b05b792b7472ca5e431adf5e2777207a
Park132 <aice7@hanmail.net>
2022-10-30 22:57:03 +0900

    5day-2-1
    이전거 버전에 새로 임포트해서 용량 많아졌을 듯
    - 건물 및 풍차
    - 덤불
    - 농장에 있는 마쉬멜로우같은건 직접 만듬

--- Project_Tank/Assets/Script/ERader_ctrl.cs
+++ Project_Tank/Assets/Script/ERader_ctrl.cs
@@ -6,16 +6,10 @@ public class ERader_ctrl : MonoBehaviour
 {
     public GameObject Parent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerStay(Collider other)
     {
         
-        if (other.transform.tag == "Player")
+        if (other.transform.tag == "Player") // 인지범위를 수치화 하고 싶은데 그냥 엔진에서 눈대중으로 대강 설정함
         {
             Parent.transform.parent.GetComponentInParent<Enemy_ctrl>().isFind = true; // 부모 오브젝트의 스크립트의 변수에 접근
             Debug.Log("check_trigger");
